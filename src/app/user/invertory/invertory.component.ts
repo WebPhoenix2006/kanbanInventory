7993a2f3ab2a46c345e17bf36ee11199b0f6c029
Viccodes <ayeniv69@gmail.com>
2025-05-27 18:56:49 +0100

    generated the inventory componnent

---
+++ src/app/user/invertory/invertory.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invertory',
+  imports: [],
+  templateUrl: './invertory.component.html',
+  styleUrl: './invertory.component.scss'
+})
+export class InvertoryComponent {
+
+}
