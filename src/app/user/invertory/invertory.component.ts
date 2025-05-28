92ac30a2f55a2a6ae40ddb979fb3a4cdf3526865
Viccodes <ayeniv69@gmail.com>
2025-05-28 11:10:57 +0100

    merged both files

--- src/app/user/invertory/invertory.component.ts
+++ src/app/user/invertory/invertory.component.ts
@@ -15,7 +15,7 @@ products = [
   { name: 'Pepsi', price: 420, quantity: '18 packets', category: '6 Packets',  date: '2025-08-22', availability: 'in stock' },
   { name: 'Fanta', price: 350, quantity: '25 packets', category: '15 Packets', date: '2025-06-15', availability: 'low stock' },
   { name: 'Sprite', price: 300, quantity: '20 packets', category: '20 Packets', date: '2025-09-01', availability: 'in stock' },
-  { name: '7Up', price: 330, quantity: '22 packets', category: '9 Packets',  date: '2025-07-30', availability: 'low stock' },   // changed here
+  { name: '7Up', price: 330, quantity: '22 packets', category: '9 Packets',  date: '2025-07-30', availability: 'low stock' },   
   { name: 'Mirinda', price: 290, quantity: '19 packets', category: '29 Packets',  date: '2025-08-18', availability: 'low stock' },
   { name: 'Mountain Dew', price: 370, quantity: '15 packets', category: '21 Packets',  date: '2025-09-05', availability: 'in stock' },
   { name: 'Lipton Ice Tea', price: 480, quantity: '10 packets', category: '4 Packets', date: '2025-07-21', availability: 'out of stock' },
