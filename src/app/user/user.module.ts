c29e5d3e4e45dc0c0ea3081edd33f9e4254dbe2f
Samuel Emeka <devsamuel0611@gmail.com>
2025-06-03 16:54:35 +0100

    added basic charts to dashboard

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -16,17 +16,7 @@ import { SuppliersComponent } from './suppliers/suppliers.component';
 import { OrdersComponent } from './orders/orders.component';
 
 @NgModule({
-<<<<<<< HEAD
   declarations: [LayoutComponent, DashboardComponent, InventoryComponent, SuppliersComponent, OrdersComponent, ProductDetailsComponent],
-=======
-  declarations: [
-    LayoutComponent,
-    DashboardComponent,
-    InventoryComponent,
-    SuppliersComponent,
-    OrdersComponent,
-  ],
->>>>>>> 514699596ecb7a77628c81f9540f1fd9e4f4bf56
   imports: [
     CommonModule,
     NgChartsModule,
